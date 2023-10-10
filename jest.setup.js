8c07e9121b74c3609de934b3428fe54cb98ff822
Koen Verheyen <verheyen.koen@gmail.com>
2023-10-10 11:45:10 +0200

    Upgraded dependencies

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,5 @@
 import 'raf/polyfill'
 import { configure } from 'enzyme'
-// temporary until official enzyme-adapter-react-17 is released
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+import Adapter from '@cfaester/enzyme-adapter-react-18'
 
 configure({ adapter: new Adapter() })
